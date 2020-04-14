e58e6ce665eb29fa7e21864faed86796c9175e7a
emtwnty <muizahsanu@gmail.com>
2020-04-14 10:51:10 +0700

    membuat tampilan login

--- app/src/main/java/com/example/akb_tugas_2/Login_Activity.kt
+++ app/src/main/java/com/example/akb_tugas_2/Login_Activity.kt
@@ -3,7 +3,7 @@ package com.example.akb_tugas_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Login_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
