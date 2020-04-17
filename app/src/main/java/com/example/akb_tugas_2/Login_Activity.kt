cac1c13f40a062e63092dd7f616442d19dcf4d1a
emtwnty <muizahsanu@gmail.com>
2020-04-17 13:03:39 +0700

    menambahkan identitas komentar pada login aktivity

--- app/src/main/java/com/example/akb_tugas_2/Login_Activity.kt
+++ app/src/main/java/com/example/akb_tugas_2/Login_Activity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+/*Tanggal Pengerjaan: 11 April 2020
+* Nama : Muiz Ahsanu Haqi
+* Kelas: IF-5
+* Nim  : 10117199*/
+
 class Login_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
